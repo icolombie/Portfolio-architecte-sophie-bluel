ef8c1bd4eaca3a241358c04342cc32fec1aff392
Isabelle Colombie <isabellecolombie@gmail.com>
2024-07-10 16:44:06 +0200

    Ajout boutons de filtre par catégories

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,3 +1,4 @@
+//Récupération dynamique des travaux
 const reponse = await fetch("http://localhost:5678/api/works");
 const works = await reponse.json();
 
@@ -16,3 +17,56 @@ function genererProjets(works) {
     }
 }
 genererProjets(works);
+
+//Ajout des boutons de tri
+const barreBoutons = document.querySelector(".barreBoutons");
+const boutonTous = document.createElement("button");
+boutonTous.innerText = "Tous";
+boutonTous.classList.add("btntous");
+barreBoutons.appendChild(boutonTous);
+const boutonObjets = document.createElement("button");
+boutonObjets.classList.add("btnObjets");
+boutonObjets.innerText = "Objets";
+barreBoutons.appendChild(boutonObjets);
+const boutonAppart = document.createElement("button");
+boutonAppart.classList.add("btnAppart");
+boutonAppart.innerText = "Appartements";
+barreBoutons.appendChild(boutonAppart);
+const boutonsHotels = document.createElement("button");
+boutonsHotels.classList.add("btnResto");
+boutonsHotels.innerText = "Hotels & Restaurants";
+barreBoutons.appendChild(boutonsHotels);
+
+
+//gestion des boutons de tri
+boutonObjets.addEventListener("click", function () {
+    const triObjets = works.filter(function (work) {
+        return work.category.name === "Objets";
+    })
+    document.querySelector(".gallery").innerHTML = "";
+    genererProjets(triObjets);
+});
+boutonAppart.addEventListener("click", function () {
+    const triObjets = works.filter(function (work) {
+        return work.category.name === "Appartements";
+    })
+    document.querySelector(".gallery").innerHTML = "";
+    genererProjets(triObjets);
+});
+boutonsHotels.addEventListener("click", function () {
+    const triObjets = works.filter(function (work) {
+        return work.category.name === "Hotels & restaurants";
+    })
+    document.querySelector(".gallery").innerHTML = "";
+    genererProjets(triObjets);
+});
+boutonTous.addEventListener("click", function () {
+    const triObjets = works.filter(function (work) {
+        return work.category.name;
+    })
+    document.querySelector(".gallery").innerHTML = "";
+    genererProjets(triObjets);
+});
+
+
+
